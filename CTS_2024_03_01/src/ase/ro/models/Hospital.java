55c411ec8a9e145cb5de589cc7a4206e16430f33
daniel-strugariu <daniel.strugariu@gmail.com>
2024-02-29 20:07:21 +0200

    Initial

---
+++ CTS_2024_03_01/src/ase/ro/models/Hospital.java
@@ -0,0 +1,4 @@
+package ase.ro.models;
+
+public class Hospital {
+}
---
+++ CTS_2024_03_01/src/ase/ro/programs/Main.java
@@ -0,0 +1,7 @@
+package ase.ro.programs;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+    }
+}
